cac1fe6abb2df2132dbc2973c6122d4e765d49ac
Jonathan Yu <yu.jona@northeastern.edu>
2023-12-08 20:07:40 -0500

    Added all the code for comments (Not Tested)

--- comments/commentsModel.js
+++ comments/commentsModel.js
@@ -1,4 +1,4 @@
 import mongoose from "mongoose";
-import schema from "./schema.js";
-const model = mongoose.model("recipes", schema);
+import commentsSchema from "./commentsSchema";
+const model = mongoose.model("comments", commentsSchema);
 export default model;
