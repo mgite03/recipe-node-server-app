026a2d4ca65ec813d9bac416561d109dc4d06d38
Jonathan Yu <yu.jona@northeastern.edu>
2023-12-10 23:17:34 -0500

    Added new variables and delete .env just in case it affects the remote server

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ import mongoose from "mongoose";
 import session from "express-session";
 
 try {
-  mongoose.connect("mongodb://127.0.0.1:27017/recipe");
+  const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas'
+  mongoose.connect(CONNECTION_STRING);
   console.log("mongoose connected");
 } catch (err) {
   console.log("not connected");
@@ -19,14 +20,19 @@ app.use(
     credentials: true,
     origin: process.env.FRONT_END_URL,
   })
- );
- 
-
+);
 const sessionOptions = {
   secret: "any string",
   resave: false,
   saveUninitialized: false,
 };
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
 app.use(session(sessionOptions));
 
 app.use(express.json());
